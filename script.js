8f024470479239e309053de5f722dbb972857968
Trần Đình Nam <trandinhnamuet@gmail.com>
2025-09-05 16:46:50 +0700

    fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const url = "assets/phutraco.pdf"; // PDF của bạn
+const url = "assets/myfile.pdf"; // PDF của bạn
 
 pdfjsLib.getDocument(url).promise.then(function(pdf) {
   const flipbook = document.getElementById("flipbook");
 
-  let loadPage = (num) => {
+  let loadedPages = 0;
+
+  function loadPage(num) {
     pdf.getPage(num).then(function(page) {
       const viewport = page.getViewport({ scale: 1.5 });
       const canvas = document.createElement("canvas");
@@ -17,20 +19,21 @@ pdfjsLib.getDocument(url).promise.then(function(pdf) {
         div.appendChild(canvas);
         flipbook.appendChild(div);
 
-        // Khi load xong tất cả thì khởi tạo turn.js
-        if (flipbook.childElementCount === pdf.numPages) {
+        loadedPages++;
+        if (loadedPages === pdf.numPages) {
+          // khi đã load đủ các trang thì mới khởi tạo turn.js
           $("#flipbook").turn({
             width: 1000,
             height: 700,
             autoCenter: true,
-            display: "double",   // <-- 2 trang cùng lúc
-            elevation: 50,
-            gradients: true
+            display: "double",
+            gradients: true,
+            elevation: 50
           });
         }
       });
     });
-  };
+  }
 
   for (let i = 1; i <= pdf.numPages; i++) {
     loadPage(i);
